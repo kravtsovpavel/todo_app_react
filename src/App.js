91cb583e1aba35634252c4e4ae2859ed0e8b6d23
Pavel Kravtsov <kravtsov1987@mail.ru>
2022-12-16 16:08:37 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Form from './components/Form/Form';
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [allTodos, setAllTodos] = useState(0);
+
+  const putTodo = (value) => {
+    if (value) {
+      setTodos([...todos, {id: Date.now(), text: value, done: false}])
+      setAllTodos(allTodos + 1)
+    } else {
+      alert('Введите текст')
+    }
+  }
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => {
+      if(todo.id !== id) return todo;
+      return {
+        ...todo,
+        done: !todo.done
+      }
+    }))
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+    setAllTodos(allTodos - 1)
+  }
+
+  const clearTodos = () => {
+    setTodos([])
+    setAllTodos(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className="container">
+        <h1 className='title'>Задачи на день</h1>
+        <Form putTodo={putTodo} />
+        <ul className='todos'> {
+          todos.map(todo => {
+            return (
+              <li className={todo.done ? 'todo done' : 'todo'} key={todo.id} onClick={e => toggleTodo(todo.id)}>{todo.text}
+              <img src='./trash.png' alt='delete' className='delete' onClick={e => {
+                e.stopPropagation();
+                removeTodo(todo.id);
+              }} />
+              </li>              
+            )
+          })
+        }
+        <div className='info'>
+          <span>Всего задач на день: {allTodos}</span>
+        </div>
+        <button className='btn' onClick={clearTodos}>Удалить все задачи</button>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './Form.css';
+
+function Form (props) {
+    const [value, setValue] = useState('');
+
+    return (
+        <form className='form' onSubmit={e => {
+            e.preventDefault();
+            props.putTodo(value);
+            setValue('');
+        }}>
+            <input type='text' placeholder='Добавьте задачу на день' 
+            className='input' value={value} onChange={e => setValue(e.target.value)} />
+        </form>
+
+    )
+}
+
+export default Form;
